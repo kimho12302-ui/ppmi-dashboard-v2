0d861e16532c5cf731b210105f33e6dde68cbbd9
ho kim <oo12302@gmail.com>
2026-03-17 16:21:08 +0900

    feat: ppmi dashboard v2

--- commit.bat
+++ commit.bat
@@ -1,3 +1,2 @@
-@echo off
-cd /d C:\Users\김호\Projects\ppmi-dashboard-v2
+cd C:\Users\김호\Projects\ppmi-dashboard-v2
 git -c user.name="ho kim" -c user.email="oo12302@gmail.com" commit -m "feat: ppmi dashboard v2"
